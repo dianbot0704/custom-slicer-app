91bd60db2f8ce7729c3a39dc3dc2cb4195e64049
Alwin Arrasyid <alwin.ridd@gmail.com>
2025-12-02 23:05:37 +0700

    feat: show full-screen layout manager

--- Extensions/IntraOperativePlan/Modules/Scripted/IntraOperativePlan/IntraOperativePlan.py
+++ Extensions/IntraOperativePlan/Modules/Scripted/IntraOperativePlan/IntraOperativePlan.py
@@ -10,6 +10,7 @@ import os
 
 PATIENT_MANAGEMENT_LAYOUT_ID = 255 + 1
 
+
 class IntraOperativePlan(ScriptedLoadableModule):
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
@@ -19,31 +20,56 @@ class IntraOperativePlan(ScriptedLoadableModule):
 
 
 class IntraOperativePlanWidget(ScriptedLoadableModuleWidget):
+    def __init__(self, parent=None):
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        self._previousLayout = None
+
     def setup(self):
-       self.patient_mgmt_layout = PatientManagementLayout(parent=None, resourcePath=self.resourcePath)
-       self.patient_mgmt_viewf = slicer.qSlicerSingletonViewFactory()
-       self.patient_mgmt_viewf.setTagName("patient_mgmt")
-       self.patient_mgmt_viewf.setWidget(self.patient_mgmt_layout)
-       lm = slicer.app.layoutManager()
-       if lm is not None:
-           lm.registerViewFactory(self.patient_mgmt_viewf)
-           layout_node = lm.layoutLogic().GetLayoutNode()
-           patient_mgmt_layout_tag = (
-                   '<layout type="horizontal">'
-                   "<item> "
-                   " <patient_mgmt></patient_mgmt> "
-                   "</item> "
-                   "</layout>"
+        self.patient_mgmt_layout = PatientManagementLayout(parent=None, resourcePath=self.resourcePath)
+        self.patient_mgmt_viewf = slicer.qSlicerSingletonViewFactory()
+        self.patient_mgmt_viewf.setTagName("patient_mgmt")
+        self.patient_mgmt_viewf.setWidget(self.patient_mgmt_layout)
+        lm = slicer.app.layoutManager()
+        if lm is not None:
+            lm.registerViewFactory(self.patient_mgmt_viewf)
+            layout_node = lm.layoutLogic().GetLayoutNode()
+            patient_mgmt_layout_tag = (
+                '<layout type="horizontal"><item><patient_mgmt></patient_mgmt></item></layout>'
             )
-           layout_node.AddLayoutDescription(PATIENT_MANAGEMENT_LAYOUT_ID, patient_mgmt_layout_tag)
+            layout_node.AddLayoutDescription(PATIENT_MANAGEMENT_LAYOUT_ID, patient_mgmt_layout_tag)
 
     def enter(self):
         lm = slicer.app.layoutManager()
         if lm is not None:
+            self._previousLayout = lm.layout
             lm.setLayout(PATIENT_MANAGEMENT_LAYOUT_ID)
+        qt.QTimer.singleShot(0, self._hideModulePanel)
+
+    def exit(self):
+        self._showModulePanel()
+        lm = slicer.app.layoutManager()
+        if lm is not None and self._previousLayout is not None:
+            lm.setLayout(self._previousLayout)
+        qt.QTimer.singleShot(0, self._showModulePanel)
+
+    def _hideModulePanel(self):
+        mainWindow = slicer.util.mainWindow()
+        if mainWindow:
+            panel = mainWindow.findChild("QDockWidget", "PanelDockWidget")
+            if panel:
+                panel.hide()
+
+    def _showModulePanel(self):
+        mainWindow = slicer.util.mainWindow()
+        if mainWindow:
+            panel = mainWindow.findChild("QDockWidget", "PanelDockWidget")
+            if panel:
+                panel.show()
+
 
 class IntraOperativePlanLogic(ScriptedLoadableModuleLogic):
     pass
+
 
 class PatientManagementLayout(qt.QWidget):
     def __init__(self, parent=None, resourcePath=None):
@@ -53,4 +79,3 @@ class PatientManagementLayout(qt.QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(ui_widget)
         self.ui = slicer.util.childWidgetVariables(ui_widget)
-
