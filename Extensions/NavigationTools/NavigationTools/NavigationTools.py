6d165a80f580adc9601973f5187cf669f860ff9d
Alwin Arrasyid <alwin.ridd@gmail.com>
2026-02-04 15:58:40 +0700

    chore: tried adding a new extension script

---
+++ Extensions/NavigationTools/NavigationTools/NavigationTools.py
@@ -0,0 +1,7 @@
+from slicer.ScriptedLoadableModule import ScriptedLoadableModule
+
+class NavigationTools(ScriptedLoadableModule):
+    def __init__(self, parent):
+        parent.title = "AIG Navigation Tools"
+        parent.categories = ["Navigation"]
+        parent.contributors = [""]
